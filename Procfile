d12cf720d38907500b6d19e7ab9d4efcbc0d9801
Diego Ampuero <diego.ampuero2@cloud.uautonoma.cl>
2025-05-12 01:32:45 -0400

    Actualizar startup.sh y Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: bash startup.sh && gunicorn app:app
-
--- startup.sh
+++ startup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-echo "Instalando dependencias necesarias..."
-apt-get update && apt-get install -y apt-transport-https
+# Instalar dependencias
+apt-get update && apt-get install -y apt-transport-https curl
+# Agregar las llaves de Microsoft para SQL Server
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# Agregar los repositorios de Microsoft
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
-echo "Instalación completada."
+# Actualizar lista de paquetes
+apt-get update
+# Aceptar el EULA e instalar ODBC Driver y herramientas de UnixODBC
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+# Verificar que se haya instalado correctamente
+echo "ODBC Driver instalado correctamente."
+
